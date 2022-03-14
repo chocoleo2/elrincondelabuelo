17ef0ea44da880b84ec5fd166b12d7457965bcd5
chocoleo2 <72154335+chocoleo2@users.noreply.github.com>
2022-03-14 04:34:19 -0600

    Create barrasearch.php

---
+++ resources/views/modulos/barrasearch.php
@@ -0,0 +1,33 @@
+
+<header class="main-header">
+
+<!-- PERFIL DE USUARIO -->
+
+<div class="navbar-custom-menu">
+    <ul class="nav navbar-nav">
+        <li class="dropdown user user-menu">
+
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <img src="img/usuarios/default/anonimo.png" class="user-image"   alt="">
+                <span class="hidden-xa">Usuario Administrador</span>
+
+            </a>
+        </li>
+    </ul>
+
+    <!-- dropdown -->
+
+<ul class="dropdown">
+    <li class="user-body">
+        <div class="pull-right">
+            <a href="" class="btn btn-default btn-flat">SALIR</a>
+        </div>
+    </li>
+
+</ul>
+
+
+
+</div>  
+
+</header>
